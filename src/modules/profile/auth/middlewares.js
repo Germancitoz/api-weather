8ab883904c2376322cea15024f0b7dd91c74d50c
German Fernandez <germanfernandez0306@gmail.com>
2022-07-08 20:53:30 -0300

    feat: check if the token is valid for a profile

--- src/modules/profile/auth/middlewares.js
+++ src/modules/profile/auth/middlewares.js
@@ -1,4 +1,5 @@
 import { verifyToken } from '../../../services/jwt.js'
+import Profile from '../Profile.js'
 
 export const isAuthenticated = async (request, response, next) => {
   const token = request.headers.authorization?.split(' ')[1] // Remove Bearer
@@ -6,8 +7,14 @@ export const isAuthenticated = async (request, response, next) => {
   if (!token) return response.status(400).json({ error: 'not token found' })
 
   try {
-    const payload = verifyToken(token)
-    request.id = payload
+    const payload_id = verifyToken(token)
+    const profile = await Profile.findById(payload_id)
+    if (!profile)
+      return response
+        .status(404)
+        .json({ error: 'This token does have no profile' })
+
+    request.id = payload_id
     next()
   } catch (error) {
     return response.status(404).json({ error: error.message })
