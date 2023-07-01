d8a42b21e9566da4ad5a896b61b5ea0bd3f87bf9
German Fernandez <germanfernandez0306@gmail.com>
2023-07-01 00:32:38 -0300

    feat: add new methods about post supabase

--- src/services/posts/index.server.ts
+++ src/services/posts/index.server.ts
@@ -6,6 +6,16 @@ export async function getPosts() {
 	return (await supabase.from('posts').select('*')).data as Post[]
 }
 
+export async function getPostById(id: Post['id']): Promise<Post | null> {
+	const { data } = await supabase.from('posts').select('*').eq('id', id)
+	return data?.[0] ?? null
+}
+
+export async function getPostsByUserId(user_id: Post['user_id'] | undefined, limit = 3) {
+	return (await supabase.from('posts').select('*').limit(limit).eq('user_id', user_id))
+		.data as Post[]
+}
+
 export async function createPost(post: PostCreate) {
 	try {
 		const validatedPost = validatePost(post)
