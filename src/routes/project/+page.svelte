55a7c50f1995d02eb588616a0c2076e53a0ea734
German Fernandez <germanfernandez0306@gmail.com>
2023-07-07 18:33:35 -0300

    feat: add project

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,1 +1,27 @@
-<h1 class="text-9xl font-bold">About</h1>
+<h1 class="mt-12 text-7xl font-bold">About</h1>
+<p class="mt-2 text-2xl">
+	This project is a simple <span class="underline decoration-wavy">blog</span> made with
+	<a
+		class="font-bold decoration-wavy transition hover:underline"
+		href="https://kit.svelte.dev/"
+		target="_blank">SvelteKit</a
+	>,
+	<a
+		class="font-bold decoration-wavy transition hover:underline"
+		href="https://supabase.com/"
+		target="_blank">Supabase</a
+	>, and
+	<a
+		class="font-bold decoration-wavy transition hover:underline"
+		href="https://tailwindcss.com/"
+		target="_blank">TailwindCSS</a
+	>, check the
+	<a
+		class="text-2xl font-bold decoration-wavy transition-all hover:underline"
+		target="_blank"
+		href="https://github.com/Germancitoz/onlyfacts">source code</a
+	>
+</p>
+<p class="mt-2 text-xl">
+	The idea is that the people could post their facts or truths that hurt or excite
+</p>
