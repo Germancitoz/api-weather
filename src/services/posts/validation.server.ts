65ac516bbcc8b0ecada229f5ddd0714de29edfae
German Fernandez <germanfernandez0306@gmail.com>
2023-07-16 15:04:37 -0300

    fix: new user_id check

--- src/services/posts/validation.server.ts
+++ src/services/posts/validation.server.ts
@@ -4,7 +4,7 @@ import type { PostCreate } from '../../types'
 const PostSchema = z.object({
 	title: z.string().min(10).max(100).trim().nonempty(),
 	body: z.string().min(40).max(300).trim().nonempty(),
-	user_id: z.number()
+	user_id: z.string()
 })
 
 export function validatePost(post: PostCreate) {
