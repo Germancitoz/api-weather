d0b238a5da47fe5fb40b49e99963454cc1ac4ef7
German Fernandez <germanfernandez0306@gmail.com>
2023-08-03 13:38:58 -0300

    feat: add your posts

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -2,6 +2,19 @@
 	export let data
 </script>
 
-{#each data.posts as post}
-	<h1>{post.title}</h1>
-{/each}
+<svelte:head>
+	<title>Your posts</title>
+</svelte:head>
+
+<h1 class="mt-12 text-7xl font-bold">Your posts #{data.posts.length}</h1>
+<ul class="mt-8 flex flex-col gap-4">
+	{#each data.posts as post, i}
+		<a
+			class="max text overflow-hidden text-ellipsis whitespace-nowrap rounded-md border-2 border-neutral-700 p-4 transition-all hover:border-white"
+			href="posts/{post.id}"
+			><span class="text-xl font-bold">#{i + 1} </span>{post.title} Lorem, ipsum dolor sit amet consectetur
+			adipisicing elit. Commodi laborum vero debitis id minus aliquid repellendus tenetur pariatur recusandae
+			consectetur fugit ipsa, ex ipsam! Reiciendis ab amet commodi dolorem enim.</a
+		>
+	{/each}
+</ul>
